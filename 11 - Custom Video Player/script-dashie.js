1c6d42912a99418d24c9f3c17b573f49b7c9b746
dash <dangerousdashie@gmail.com>
2019-02-16 14:57:32 -0500

    Finished HTML5 custom video but still have to finish extra challenge to make a full screen button

--- 11 - Custom Video Player/script-dashie.js
+++ 11 - Custom Video Player/script-dashie.js
@@ -2,13 +2,14 @@
 const player = document.querySelector('.player');
 const vidElem = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress_filled');
+const progressBar = player.querySelector('.progress__filled');
 
 
 const toggle = player.querySelector('.toggle');
 const play = player.querySelector('[title="Toggle Play"]');
-const skipButtons = player.querySelector('[dataSkip]');
-const ranges= player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges= player.querySelectorAll('.player__slider');
+const ranges= player.querySelector('.full_screen');
 
 
 // build functions
@@ -19,14 +20,44 @@ function togglePlay(){
 function updateButton(){
     console.log(this.paused, " play/pause");
     const icon = this.paused? '►' : '❚ ❚';
-    
-    console.log(icon);
     toggle.textContent = icon;
-
 }
+
+function skip(){
+    vidElem.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+    vidElem[this.name] = this.value;
+   // debugger;
+}
+
+function handleProgress(e){
+    let percent = vidElem.currentTime/vidElem.duration * 100 + '%';
+    progressBar.style.flexBasis = percent;
+}
+let mousedown = false;
+function setFlag(e){
+   e.type == 'mousedown'? mousedown = true: mousedown = false;
+}
+function scrub(e){
+    if(e.type == 'mousemove'&& mousedown == false) return;
+    let fraction = e.layerX/this.clientWidth; 
+    vidElem.currentTime = fraction * vidElem.duration;
+}
+
+
 // hook up event listeners 
 
 vidElem.addEventListener('click', togglePlay);
 vidElem.addEventListener('play', updateButton);
 vidElem.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach((x)=> x.addEventListener('click', skip));
+ranges.forEach((x)=> x.addEventListener('change', handleRangeUpdate));
+vidElem.addEventListener('timeupdate', handleProgress);
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', scrub);
+progress.addEventListener('mousedown', setFlag);
+progress.addEventListener('mouseup', setFlag);
+//progress.addEventListener('mouseout', setFlag);
